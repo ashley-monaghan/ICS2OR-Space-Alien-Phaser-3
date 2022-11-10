f4d364de9db9c6add15604452b264c2f5001d9c7
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-11-10 19:30:28 +0000

    fix game.js

--- js/game.js
+++ js/game.js
@@ -1,9 +1,28 @@
 /* global Phaser */
+
+// Copyright (c) 2022 Ashley Monaghan all rights reserved
+//
+// Created by: Ashley Monaghan
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // set background colour
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaer.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
 
 const game = new Phaser.Game(config)
