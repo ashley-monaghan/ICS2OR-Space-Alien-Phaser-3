0ee71071652ad344378a07e4dc56e6642a800873
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-11-20 20:59:12 +0000

    this.scene.switch("titleScene")

--- js/splashScene.js
+++ js/splashScene.js
@@ -51,7 +51,7 @@ class SplashScene extends Phaser.Scene {
   * @param {number} delta - The delta time in ms since the last frame.
   */
   update(time, delta) {
-    // pass
+    this.scene.switch("titleScene")
   }
 }
 
